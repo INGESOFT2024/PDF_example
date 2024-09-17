8eb2000851c54c3cb1317bae4f663e283d8bf980
lucasartal56 <lucdanart@gmail.com>
2024-09-17 08:13:24 -0600

    agregar php mailer

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php 
 require_once __DIR__ . '/../includes/app.php';
 
-
+use Controllers\EmailController;
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\ReporteController;
@@ -12,6 +12,8 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 $router->get('/', [AppController::class,'index']);
 
 $router->get('/pdf', [ReporteController::class,'pdf']);
+
+$router->get('/email', [EmailController::class, 'email']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
